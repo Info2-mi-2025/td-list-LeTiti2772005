8e8ccccb5dc5a71fd2db28ff28ad111a20cb0cb6
Andrea Sisto <sisto.andrea@reno-cardan.ch>
2025-04-09 13:47:54 +0200

    Traitement des arguments OK

--- main.c
+++ main.c
@@ -39,6 +39,11 @@ void init_file()
 }
 
 
+#define NOFILE 2
+#define ARGERROR 1
+
+#define MAXFILENAME 80
+
 
 typedef struct Node
 {
@@ -51,6 +56,19 @@ typedef struct
     Node* head;
     Node* tail;
 } List;
+
+typedef struct 
+{
+    bool isReverse;
+    bool isSum;
+    bool isFilter;
+    int filterValue;
+    bool isAdd;
+    int addValue;
+    bool foundName;
+    char filename[MAXFILENAME];
+}Options;
+
 
 // Fonctions de base
 void append(List* list, int value)
@@ -102,6 +120,11 @@ void help()
     printf("  --max             Affiche la valeur maximale de la liste\n");
 }
 
+void version()
+{
+    printf("version 1.0");
+}
+
 // Lecture fichier
 bool read_file(const char* filename, List* list)
 {
@@ -124,13 +147,73 @@ bool add_to_file(const char* filename, int value)
     return true;
 }
 
+bool parseArgs(int argc, char* argv[], Options* opt)
+{
+    bool foundName = false;
+    for(int i = 1; i<argc; i++) //each arg
+    {
+        if(strcmp(argv[i], "--reverse") == 0)
+        {
+            opt->isReverse = true;
+            continue;
+        }
+        else if(strcmp(argv[i], "--sum")== 0)
+        {
+            opt->isSum = true;
+            continue;
+        }
+        else if(strcmp(argv[i], "--filter")== 0)
+        {
+            opt->isFilter = true;
+            if(scanf(argv[i], "--filter%d", opt->filterValue) != 1)return(false);
+            continue;
+        }
+        else if(strcmp(argv[i], "--add")== 0)
+        {
+            opt->isAdd = true;
+            if(scanf(argv[i], "--filter%d", opt->addValue) != 1)return(false);
+            continue;
+        }
+        else if(strcmp(argv[i], "--help")== 0)
+        {
+            help();
+            exit(0);
+        }
+        else if(strcmp(argv[i], "-v")== 0 || strcmp(argv[i], "--version")== 0)
+        {
+            version();
+            exit(0);   
+        }
+
+        //not an option -> filename
+        if(!opt->foundName)
+        {
+            foundName = true;
+            strcpy(opt->filename, argv[i]);
+        }
+
+    }
+    return(foundName);
+}
+
+
 int main(int argc, char* argv[])
 {
     // Ne pas modifier
     init_file();
     // ---------------
 
-    if(argc < 2) return 1;
+    if(argc < 2) return ARGERROR;
+    Options options = {0};
+    const bool argRet = parseArgs(argc, argv, &options);
+    if(!argRet)return(ARGERROR);
+
+    FILE* fp = fopen(options.filename, "r");
+    if(fp == NULL)return(NOFILE);
+    fclose(fp);
+
+
+
     
     return 0;
 }
